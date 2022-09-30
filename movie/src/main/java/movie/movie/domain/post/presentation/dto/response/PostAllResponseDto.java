07d1efe80bb629af4c23e6a8597529dfe3a4c553
nswon <swnam0729@gmail.com>
2022-09-30 19:45:59 +0900

    feat : 영화 댓글 도메인

--- movie/src/main/java/movie/movie/domain/post/presentation/dto/response/PostAllResponseDto.java
+++ movie/src/main/java/movie/movie/domain/post/presentation/dto/response/PostAllResponseDto.java
@@ -6,12 +6,14 @@ import movie.movie.domain.post.domain.Post;
 @Getter
 public class PostAllResponseDto {
 
+    private final Long id;
     private final String imgPath;
     private final String title;
     private final double average;
     private final String movieDate;
 
     public PostAllResponseDto(Post post) {
+        this.id = post.getId();
         this.imgPath = post.getImgPath();
         this.title = post.getTitle();
         this.average = post.getAverage();
